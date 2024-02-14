067e3a7c303be47d1383c25620e081a617709d5a
vietdh <vietdanghuy.95@gmail.com>
2024-02-14 12:45:54 +0700

    Init and create the github authentication

---
+++ src/app/providers.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { NextUIProvider } from "@nextui-org/react";
+
+interface Providers {
+    children: React.ReactNode
+}
+
+export default function Providers({ children }: Providers) {
+    return (
+        <NextUIProvider>
+            {children}
+        </NextUIProvider>
+    )
+}
