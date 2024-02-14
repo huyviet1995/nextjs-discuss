df16e92225c3006645b8524da49a6cf9476fe1ca
vietdh <vietdanghuy.95@gmail.com>
2024-02-14 23:57:16 +0700

    Use zod to display errors after submitting invalid form

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,34 @@
 'use server';
 
-export async function createTopic() {
-    // TODO: revalidate the homepage
-    return true;
+import { z } from 'zod';
+
+const createTopicSchema = z.object({
+    name: z.string().min(3).max(255).regex(/[a-z-]/, { message: 'Must be lowercase and contain a-z characters, or dots without spaces' }),
+    description: z.string().min(3).max(255)
+})
+
+interface CreateTopicFormState {
+    errors: {
+        name?: string[];
+        description?: string[];
+        _form?: string[];
+    }
+}
+
+export async function createTopic(formState: CreateTopicFormState, formData: FormData): Promise<CreateTopicFormState> {
+    const name = formData.get('name') as string;
+    const description = formData.get('description') as string;
+
+    const result = createTopicSchema.safeParse({ name, description });
+
+    if (!result.success) {
+        const errors = result.error.flatten().fieldErrors;
+        return {
+            errors
+        }
+    }
+
+    return {
+        errors: {}
+    }
 }
